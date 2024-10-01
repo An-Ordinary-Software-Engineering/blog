b9b0930d60832f6db3cb87092c37bc48644b2e35
csvitor-dev <csvitor.dev@gmail.com>
2024-10-01 18:28:08 -0300

    style: building the base styling and defining custom 'CSS' utility classes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			"b-aose": "#0f0f0f",
+			"w-aose": "#f0f0f0",
+			"g-aose": "#e5e5e5",
+		},
 		extend: {},
 	},
 	plugins: [],
