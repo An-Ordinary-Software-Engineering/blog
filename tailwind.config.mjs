a17166b274a280e59f6bd07592582d7f800b091b
csvitor-dev <csvitor.dev@gmail.com>
2024-10-02 11:05:10 -0300

    style: new 'tailwind' settings

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			"b-aose": "#0f0f0f",
-			"w-aose": "#f0f0f0",
-			"g-aose": "#e5e5e5",
-		},
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "b-aose": "#0f0f0f",
+      "w-aose": "#f0f0f0",
+      "g-aose": "#e5e5e5",
+      "easy": "#7bff6a",
+      "medium": "#fffb7d",
+      "hard": "#ff7d7d",
+    },
+    extend: {
+      boxShadow: {
+        post: "7px 6px 9px 1px #00000025",
+      },
+    },
+  },
+  plugins: [],
+};
